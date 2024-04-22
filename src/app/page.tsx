4c67dc6a92a1a92c604b92974299608fafc7552a
vitalyvitmens <otm_@tut.by>
2024-04-22 21:24:24 +0300

    chore: add prettier and shadcn/ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../shared/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>Button</Button>
+    </main>
+  );
+}
