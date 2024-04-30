36ac675e029421ebbc40875e7d39149e20d7ebb6
vitalyvitmens <otm_@tut.by>
2024-04-30 15:36:11 +0300

    fix: delete h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { CoursesList } from "../features/courses-list/pub/courses-list";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1 className="text-3xl font-bold">Courses</h1>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
