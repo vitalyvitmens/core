d82644bc95c52ba9150254072dcdf485f7f6b1da
vitalyvitmens <otm_@tut.by>
2024-05-03 14:32:34 +0300

    ci: prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { CoursesList } from "../features/courses-list/pub/courses-list";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1 className="text-3xl font-bold">Courses New</h1>
+      <h1 className="text-3xl font-bold">Courses Prod</h1>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
