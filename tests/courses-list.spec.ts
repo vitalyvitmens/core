39d19db6e560ea3d18b4405952e765bbe52d9ac5
vitalyvitmens <otm_@tut.by>
2024-05-03 23:36:13 +0300

    delete: sentry

--- tests/courses-list.spec.ts
+++ tests/courses-list.spec.ts
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 test("create delete course list", async ({ page }) => {
   await page.goto(process.env.TEST_ENV_BASE_URL || "http://localhost:3000");
-  await page.getByPlaceholder("название").click();
-  await page.getByPlaceholder("название").fill("Test course");
-  await page.getByPlaceholder("описание").click();
-  await page.getByPlaceholder("описание").fill("Test description");
+  await page.getByPlaceholder("название...").click();
+  await page.getByPlaceholder("название...").fill("Test course");
+  await page.getByPlaceholder("описание...").click();
+  await page.getByPlaceholder("описание...").fill("Test description");
   await page.getByRole("button", { name: "Добавить" }).click();
   await expect(
     page.getByText("Test courseTest descriptionУдалить"),
