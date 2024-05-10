56ff892ce2e063520c4325b04d3331f052a73ea5
vitalyvitmens <otm_@tut.by>
2024-05-10 15:49:30 +0300

    feat: add profile preview

---
+++ src/features/update-profile/update-profile-form.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { ProfileForm } from "./_ui/profile-form";
+import { Spinner } from "@/shared/ui/spinner";
+import { useRouter } from "next/navigation";
+
+export function UpdateProfileForm({
+  userId,
+  callbackUrl,
+}: {
+  userId: string;
+  callbackUrl?: string;
+}) {
+  if (false) {
+    return <Spinner aria-label="Загрузка профиля" />;
+  }
+  if (false) {
+    return <div>Не удалось загрузить профиль, возможно у вас нет прав</div>;
+  }
+  return (
+    <ProfileForm
+      // profile={profile}
+      // onSuccess={handleSuccess}
+      submitText={callbackUrl ? "Продолжить" : "Сохранить"}
+    />
+  );
+}
